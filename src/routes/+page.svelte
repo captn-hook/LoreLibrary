688c8408ec042232982083ae538a293ce71ba51a
nathan.j.sweet852@gmail.com <nathan.j.sweet852@gmail.com>
2025-04-08 14:22:27 -0700

    component library struggles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
+<script>
+    import { Button } from '@skeletonlabs/skeleton-svelte/components';
+</script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<Button> hey </Button>
