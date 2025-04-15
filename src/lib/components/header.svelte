90f8cc8ee5b5ffd1e9c06bbe8fa0a3c04046ed89
thook <tristanskyhook@gmail.com>
2025-04-15 14:47:18 -0700

    header link

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,6 +1,6 @@
 <script>
-  import { login, token, logout } from '$lib/context/userContext.svelte.js';
-  let localToken = '';
+  import { login, token, logout } from "$lib/context/userContext.svelte.js";
+  let localToken = "";
   token.subscribe((value) => {
     localToken = value;
   });
@@ -8,12 +8,17 @@
 
 <div class="header p-4">
   <div class="flex justify-between items-center">
-    <div class="flex items-center space-x-4">
-      <img src="/images/logo.png" alt="Logo" class="h-12 w-auto">
+    <a href="/" class="flex items-center space-x-4">
+      <img src="/images/logo.png" alt="Logo" class="h-12 w-auto" />
       <h1 class="text-xl font-bold">Lore Library</h1>
-    </div>
-      <button type="button" class="btn preset-tonal-primary" on:click={() => localToken && localToken.trim() != '' ? logout() : login('token')}>
-        {localToken && localToken.trim() != '' ? 'Log Out' : 'Log In'}
-      </button>
-    </div>
+    </a>
+    <button
+      type="button"
+      class="btn preset-tonal-primary"
+      on:click={() =>
+        localToken && localToken.trim() != "" ? logout() : login("token")}
+    >
+      {localToken && localToken.trim() != "" ? "Log Out" : "Log In"}
+    </button>
+  </div>
 </div>
