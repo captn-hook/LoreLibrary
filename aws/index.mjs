8fcb983a04a010bda8d70e700204464484d3d636
Tristan H <tristanskyhook@gmail.com>
2025-04-19 19:18:50 -0700

    api basics

--- aws/index.mjs
+++ aws/index.mjs
@@ -19,6 +19,131 @@ function notImplemented(name) {
     };
 }
 
+function notFound(name) {
+    return {
+        statusCode: 404,
+        body: JSON.stringify({ message: `${name} not found` })
+    };
+}
+
+function badRequest(name) {
+    return {
+        statusCode: 400,
+        body: JSON.stringify({ message: `${name} bad request` })
+    };
+}
+
+class User {
+    constructor(username, content=[], worlds=[]) {
+        this.username = username; // string
+        this.content = content; // array of strings
+        this.worlds = worlds; // array of strings
+    }
+}
+class World {
+    constructor(name, id, content=[], tags=[], parentId=null, collections=[]) {
+        this.name = name; // string
+        this.id = id; // string
+        this.content = content; // array of strings
+        this.tags = tags; // array of strings
+        this.parentId = parentId; // string
+        this.collections = collections; // array of strings
+    }
+}
+class Collection {
+    constructor(name, id, content=[], tags=[], parentId=null, collections=[], entries=[]) {
+        this.name = name; // string
+        this.id = id; // string
+        this.content = content; // array of strings
+        this.tags = tags; // array of strings
+        this.parentId = parentId; // string
+        this.collections = collections; // array of strings
+        this.entries = entries; // array of strings
+    }
+}
+class Entry {
+    constructor(name, id, content=[], tags=[], parentId=null, collections=[], resources=[]) {
+        this.name = name; // string
+        this.id = id; // string
+        this.content = content; // array of strings
+        this.tags = tags; // array of strings
+        this.parentId = parentId; // string
+        this.collections = collections; // array of strings
+        this.resources = resources; // array of strings
+    }
+}
+
+function dynamo_get(table, cls, key) {
+    const params = {
+        TableName: table,
+        Key: key
+    };
+    return ddbDocClient.send(new GetCommand(params))
+        .then(data => {
+            if (data.Item) {
+                return new cls(...Object.values(data.Item));
+            } else {
+                throw new Error('Item not found');
+            }
+        })
+        .catch(err => {
+            console.error(err);
+            throw err;
+        });
+}
+
+function dynamo_put(table, cls, item) {
+    const params = {
+        TableName: table,
+        Item: item
+    };
+    return ddbDocClient.send(new PutCommand(params))
+        .then(data => {
+            return item;
+        })
+        .catch(err => {
+            console.error(err);
+            throw err;
+        });
+}
+
+function dynamo_update(table, cls, key, update) {
+    const params = {
+        TableName: table,
+        Key: key,
+        UpdateExpression: update.expression,
+        ExpressionAttributeValues: update.values,
+        ReturnValues: "ALL_NEW"
+    };
+    return ddbDocClient.send(new UpdateCommand(params))
+        .then(data => {
+            if (data.Attributes) {
+                return new cls(...Object.values(data.Attributes));
+            } else {
+                throw new Error('Item not found');
+            }
+        })
+        .catch(err => {
+            console.error(err);
+            throw err;
+        });
+}
+
+function dynamo_delete(table, key) {
+    const params = {
+        TableName: table,
+        Key: key
+    };
+    return ddbDocClient.send(new DeleteCommand(params))
+        .then(data => {
+            return data;
+        })
+        .catch(err => {
+            console.error(err);
+            throw err;
+        });
+}
+
 export const handler = async (e) => {
     // The event object contains:
     // version: '2.0',
@@ -32,101 +157,191 @@ export const handler = async (e) => {
     try {
         const [operation, path] = e.routeKey.split(' ');
 
-        if (path === '/users' && operation == 'GET') {
-            return notImplemented('Get users');
+        // availabe routes from ./api.yaml:
+        // /worlds: GET, PUT
+        if (operation === 'GET' && path === '/worlds') {
+            // Get all worlds
+            const params = {
+                TableName: tablename,
+                ProjectionExpression: 'id, name'
+            };
+            const data = await ddbDocClient.send(new ScanCommand(params));
+            return {
+                statusCode: 200,
+                body: JSON.stringify(data.Items)
+            };
         }
-
-        if (path === '/users' && operation == 'POST') {
-            const { userId, password } = JSON.parse(body);
-            return notImplemented(`Register user: ${result}`);
+        // /signup: POST
+        else if (operation === 'POST' && path === '/signup') {
+            // Create a new user
+            const { username } = JSON.parse(e.body);
+            const user = new User(username);
+            await dynamo_put(tablename, User, user);
+            return {
+                statusCode: 200,
+                body: JSON.stringify(user)
+            };
         }
-
-        if (path.startsWith('/users/') && operation == 'GET') {
-            const { userId } = pathParameters;
-            return notImplemented(`Get user details for ${userId}`);
+        // /login: POST
+        else if (operation === 'POST' && path === '/login') {
+            // Login user
+            const { username } = JSON.parse(e.body);
+            const user = await dynamo_get(tablename, User, { username });
+            return {
+                statusCode: 200,
+                body: JSON.stringify(user)
+            };
         }
-
-        if (path.startsWith('/users/') && operation == 'PATCH') {
-            const { userId } = pathParameters;
-            const userDetails = JSON.parse(body);
-            return notImplemented(`Update user details for ${userId}`);
+        // /users: GET
+        else if (operation === 'GET' && path === '/users') {
+            return notImplemented('Get all users');
         }
-
-        if (path.startsWith('/users/') && operation == 'DELETE') {
-            const { userId } = pathParameters;
-            return notImplemented(`Delete user ${userId}`);
+        // /users/{UserId}: GET, POST, DELETE
+        else if (operation === 'GET' && path.startsWith('/users/')) {
+            const userId = path.split('/')[2];
+            return notImplemented('Get user by ID: ' + userId);
         }
-
-        if (path === '/worlds' && operation == 'GET') {
-            const userId = queryStringParameters.userId;
-            return notImplemented(`Get worlds for user ${userId}`);
+        else if (operation === 'POST' && path.startsWith('/users/')) {
+            const userId = path.split('/')[2];
+            return notImplemented('Update user by ID: ' + userId);
         }
-
-        if (path === '/worlds' && operation == 'POST') {
-            const { worldId, userId } = JSON.parse(body);
-            return notImplemented(`Create world ${worldId} for user ${userId}`);
+        else if (operation === 'DELETE' && path.startsWith('/users/')) {
+            const userId = path.split('/')[2];
+            return notImplemented('Delete user by ID: ' + userId);
         }
-
-        if (path.startsWith('/worlds/') && operation == 'GET') {
-            const { worldId } = pathParameters;
-            return notImplemented(`Get world details for ${worldId}`);
+        // NOT IMPLEMENTED
+        // /search: anything at all related to search
+        // /resources: anything at all related to resources (images, files, etc)
+        pathsplit = path.split('/');
+        // /{WorldId}: GET, POST, DELETE
+        if (pathsplit.length === 2) {
+            const worldId = pathsplit[1];
+            if (operation === 'GET') {
+                // Get world by ID
+                const world = await dynamo_get(tablename, World, { id: worldId });
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify(world)
+                };
+            } else if (operation === 'POST') {
+                // Update world by ID
+                const world = new World(...Object.values(JSON.parse(e.body)));
+                await dynamo_update(tablename, World, { id: worldId }, {
+                    expression: 'SET #name = :name',
+                    values: {
+                        ':name': world.name
+                    }
+                });
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify(world)
+                };
+            } else if (operation === 'DELETE') {
+                // Delete world by ID
+                await dynamo_delete(tablename, { id: worldId });
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify({ message: 'World deleted' })
+                };
+            }
         }
-
-        if (path.startsWith('/worlds/') && operation == 'PATCH') {
-            const { worldId } = pathParameters;
-            const worldDetails = JSON.parse(body);
-            return notImplemented(`Update world details for ${worldId}`);
+        // /{WorldId}/{CollectionId}: GET, POST, DELETE
+        else if (pathsplit.length === 3) {
+            const worldId = pathsplit[1];
+            const collectionId = pathsplit[2];
+            if (operation === 'GET') {
+                // Get collection by ID
+                const collection = await dynamo_get(tablename, Collection, { id: collectionId });
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify(collection)
+                };
+            } else if (operation === 'POST') {
+                // Update collection by ID
+                const collection = new Collection(...Object.values(JSON.parse(e.body)));
+                await dynamo_update(tablename, Collection, { id: collectionId }, {
+                    expression: 'SET #name = :name',
+                    values: {
+                        ':name': collection.name
+                    }
+                });
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify(collection)
+                };
+            } else if (operation === 'DELETE') {
+                // Delete collection by ID
+                await dynamo_delete(tablename, { id: collectionId });
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify({ message: 'Collection deleted' })
+                };
+            }
         }
-
-        if (path.startsWith('/worlds/') && operation == 'DELETE') {
-            const { worldId } = pathParameters;
-            return notImplemented(`Delete world ${worldId}`);
+        // /{WorldId}/{CollectionId}/entries: GET, PUT
+        else if (pathsplit.length === 4 && pathsplit[3] === 'entries') {
+            const worldId = pathsplit[1];
+            const collectionId = pathsplit[2];
+            if (operation === 'GET') {
+                // Get all entries in collection
+                const params = {
+                    TableName: tablename,
+                    ProjectionExpression: 'id, name',
+                    FilterExpression: 'worldId = :worldId AND collectionId = :collectionId',
+                    ExpressionAttributeValues: {
+                        ':worldId': worldId,
+                        ':collectionId': collectionId
+                    }
+                };
+                const data = await ddbDocClient.send(new ScanCommand(params));
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify(data.Items)
+                };
+            } else if (operation === 'PUT') {
+                // Create new entry in collection
+                const entry = new Entry(...Object.values(JSON.parse(e.body)));
+                await dynamo_put(tablename, Entry, entry);
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify(entry)
+                };
+            }
         }
-
-        if (path.startsWith('/worlds/') && path.endsWith('/collections') && operation == 'GET') {
-            const { worldId } = pathParameters;
-            const userId = queryStringParameters.userId;
-            return notImplemented(`Get collections for world ${worldId} and user ${userId}`);
-        }
-
-        if (path.startsWith('/worlds/') && path.endsWith('/collections') && operation == 'POST') {
-            const { worldId } = pathParameters;
-            const { collectionId, userId } = JSON.parse(body);
-            return notImplemented(`Create collection ${collectionId} for world ${worldId} and user ${userId}`);
-        }
-
-        if (path.startsWith('/worlds/') && path.includes('/collections/') && operation == 'GET') {
-            const { worldId, collectionId } = pathParameters;
-            return notImplemented(`Get collection details for world ${worldId} and collection ${collectionId}`);
-        }
-
-        if (path.startsWith('/worlds/') && path.includes('/collections/') && operation == 'PATCH') {
-            const { worldId, collectionId } = pathParameters;
-            const collectionDetails = JSON.parse(body);
-            return notImplemented(`Update collection details for world ${worldId} and collection ${collectionDetails}`);
-        }
-
-        if (path.startsWith('/worlds/') && path.includes('/collections/') && operation == 'DELETE') {
-            const { worldId, collectionId } = pathParameters;
-            return notImplemented(`Delete collection ${collectionId} for world ${worldId}`);
-        }
-
-        if (path.startsWith('/worlds/') && path.includes('/entries') && operation == 'GET') {
-            const { worldId, collectionId } = pathParameters;
-            return notImplemented(`Get entries for world ${worldId} and collection ${collectionId}`);
-        }
-
-        if (path.startsWith('/worlds/') && path.includes('/entries') && operation == 'POST') {
-            const { worldId, collectionId } = pathParameters;
-            const { entryId, userId, content } = JSON.parse(body);
-            return notImplemented(`Create entry ${entryId} for world ${worldId}, collection ${collectionId}, and user ${userId}`);
-        }
-
-        if (path.startsWith('/worlds/') && path.includes('/resources') && operation == 'POST') {
-            const { worldId } = pathParameters;
-            const { resourceId, fileContent, fileType, userId } = JSON.parse(body);
-            return notImplemented(`Upload resource ${resourceId} for world ${worldId} and user ${userId}`);
-        }
+        // /{WorldId}/{CollectionId}/{EntryId}: GET, POST, DELETE
+        else if (pathsplit.length === 5) {
+            const worldId = pathsplit[1];
+            const collectionId = pathsplit[2];
+            const entryId = pathsplit[3];
+            if (operation === 'GET') {
+                // Get entry by ID
+                const entry = await dynamo_get(tablename, Entry, { id: entryId });
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify(entry)
+                };
+            } else if (operation === 'POST') {
+                // Update entry by ID
+                const entry = new Entry(...Object.values(JSON.parse(e.body)));
+                await dynamo_update(tablename, Entry, { id: entryId }, {
+                    expression: 'SET #name = :name',
+                    values: {
+                        ':name': entry.name
+                    }
+                });
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify(entry)
+                };
+            } else if (operation === 'DELETE') {
+                // Delete entry by ID
+                await dynamo_delete(tablename, { id: entryId });
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify({ message: 'Entry deleted' })
+                };
+            }
+        }       
 
         return {
             statusCode: 404,
