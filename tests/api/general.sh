5b43f3c37b8624d980e918f3ec94c3cc95fa2dbd
Tristan H <tristanskyhook@gmail.com>
2025-04-21 19:56:50 -0700

    aws big and heavy like my

--- tests/api/general.sh
+++ tests/api/general.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define variables
-url="https://vzcambvr1l.execute-api.us-west-2.amazonaws.com/prod"
+url="https://zwfjlbrqse.execute-api.us-west-2.amazonaws.com/prod"
 token=""
 username=""
 
