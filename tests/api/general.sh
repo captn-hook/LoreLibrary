898d6b59225ff891415c981fad3ee82c9a170f8a
Tristan Sky Hook <tristanskyhook@gmail.com>
2025-05-04 16:10:46 -0700

    uh

--- tests/api/general.sh
+++ tests/api/general.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define variables
-url="https://6uptpgc8rc.execute-api.us-west-2.amazonaws.com/prod"
+url="https://zkw98x8jae.execute-api.us-west-2.amazonaws.com/prod"
 token=""
 username=""
 
