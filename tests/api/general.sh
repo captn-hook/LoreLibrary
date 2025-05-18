5ec62375719ebd7b88481348d12b4f22725ed9e8
nate <nathan.j.sweet852@gmail.com>
2025-05-18 15:33:25 -0700

    router reworked and bug fixed, now controlled by navbar and self instead of getWorld, collection or entry

--- tests/api/general.sh
+++ tests/api/general.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define variables
-url="https://r9z3eee7al.execute-api.us-west-2.amazonaws.com/prod"
+url="https://86vprgw5q5.execute-api.us-west-2.amazonaws.com/prod"
 token=""
 username=""
 email1="hookt@oregonstate.edu"
