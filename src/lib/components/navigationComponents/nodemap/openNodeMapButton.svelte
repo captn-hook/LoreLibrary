f40a6e45d57195501f1f6c5138ec9cf94ca039fc
Nate S <nathan.j.sweet852@gmail.com>
2026-01-28 13:02:50 -0800

    nodemap button disabled when any edit options dialog is active

--- src/lib/components/navigationComponents/nodemap/openNodeMapButton.svelte
+++ src/lib/components/navigationComponents/nodemap/openNodeMapButton.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 import { Waypoints } from "lucide-svelte";
-
+import { showStyleControls, editContent, editComponentContents, showCreateCollection, showCreateEntry, showDeleteItem } from "$lib/state/editState.svelte";
 export let onClick: () => void;
 </script>
 
@@ -8,6 +8,7 @@ export let onClick: () => void;
     class="btn preset-tonal-primary border-[1px] border-surface-200-800 card-hover px-2 mr-2 rounded-md h-[100%] w-auto text-base md:text-lg lg:text-xl"
     style="aspect-ratio: 1 / 1;"
     on:click={onClick}
+    disabled= {$editContent || $showStyleControls || $showCreateCollection || $showCreateEntry || $showDeleteItem}
 >
     <Waypoints class="h-6 w-6 " />
 </button>
