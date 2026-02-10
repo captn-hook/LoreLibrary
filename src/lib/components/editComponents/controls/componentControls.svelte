df2d188bd44df51537a030ee71a8c63c1a4848e1
Nate S <nathan.j.sweet852@gmail.com>
2026-02-10 11:58:10 -0800

    style button opens menu and control buttons disable eahother as needed

--- src/lib/components/editComponents/controls/componentControls.svelte
+++ src/lib/components/editComponents/controls/componentControls.svelte
@@ -2,10 +2,13 @@
     import DeleteComponentButton from "./deleteComponentButton.svelte";
     import StyleComponentButton from "./styleComponentButton.svelte";
     export let index: number;
+    let disabledOwner: string | null = null;
+
+    
 </script>
 <div class="space-y-2">
-    <DeleteComponentButton index={index} />
-    <StyleComponentButton index={index}/>
+    <DeleteComponentButton index={index} bind:disabledOwner={disabledOwner}/>
+    <StyleComponentButton index={index} bind:disabledOwner={disabledOwner}/>
 </div>
 
 
--- src/lib/components/editComponents/controls/styleComponentButton.svelte
+++ src/lib/components/editComponents/controls/styleComponentButton.svelte
@@ -1,10 +1,46 @@
 <script lang="ts">
-    export let index;
+    export let index: number;
+    export let disabledOwner: string | null = null;
+
+    import StyleComponentMenu from './syleComponentMenu.svelte';
+    let menuVisible = false;
+    let menuPosition = { x: 0, y: 0 };
+
+    function openMenu(event: MouseEvent) {
+        menuPosition = { x: event.pageX, y: event.pageY };
+        menuVisible = true;
+        disabledOwner = 'style';
+        document.addEventListener('click', handleOutsideClick);
+    }
+
+    function closeMenu() {
+        menuVisible = false;
+        if (disabledOwner === 'style') disabledOwner = null;
+        document.removeEventListener('click', handleOutsideClick);
+    }
+
+    function handleOutsideClick(event: MouseEvent) {
+        const menuElement = document.querySelector('.add-component-menu');
+        if (menuElement && !menuElement.contains(event.target as Node)) {
+            closeMenu();
+        }
+    }
 </script>
 
 <div class="style-component">
     <button 
-    class="btn preset-tonal-primary button-filled w-full">
+    disabled={disabledOwner === 'delete'}
+    class="btn preset-tonal-primary button-filled w-full"
+     on:click={(event) => { event.stopPropagation(); openMenu(event); }}>
         Style
     </button>
 </div>
+
+{#if menuVisible}
+    <StyleComponentMenu
+        {index}
+        {menuPosition}
+        {closeMenu}
+        on:closeMenu={closeMenu}
+    />
+{/if}
